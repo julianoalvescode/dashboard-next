8c0d8a63c0f8e6be30dea6795a22eb31726cffa5
Juliano Alves <contato@julianoalves.me>
2024-04-08 10:43:47 -0300

    feat: 🎸 add logo in sidebar

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,20 @@
+import { Search } from "lucide-react";
+import { Logo } from "./Logo";
+
+export function Sidebar() {
+  return (
+    <>
+      <aside className="border-r border-zinc-200 px-5 py-8 space-y-6">
+        <Logo />
+        <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
+          <Search className="h-5 w-5 text-zinc-500" />
+          <input
+            type="search"
+            placeholder="Search"
+            className="flex-1 border-0 bg-transparent p-0 focus:outline-none text-zinc-900 placeholder-zinc-600"
+          />
+        </div>
+      </aside>
+    </>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        juliano: "#FF0000",
+      },
+      maxWidth: {
+        app: "700px",
+      },
+      gridTemplateColumns: {
+        app: "minmax(18rem, 20rem) 1fr",
       },
     },
   },
