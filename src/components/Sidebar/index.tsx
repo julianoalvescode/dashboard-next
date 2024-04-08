31d08338b1ae8bbd66927dbdfcec65d62e76ddc5
Juliano Alves <contato@julianoalves.me>
2024-04-08 16:25:29 -0300

    feat: 🎸 add nav-items in sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,12 +1,13 @@
 import { Search } from "lucide-react";
 import { Logo } from "./Logo";
+import { MainNavigation } from "./MainNavigation";
 
 export function Sidebar() {
   return (
     <>
       <aside className="border-r border-zinc-200 px-5 py-8 space-y-6">
         <Logo />
-        <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
+        <div className="flex mx-1 w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
           <Search className="h-5 w-5 text-zinc-500" />
           <input
             type="search"
@@ -14,6 +15,7 @@ export function Sidebar() {
             className="flex-1 border-0 bg-transparent p-0 focus:outline-none text-zinc-900 placeholder-zinc-600"
           />
         </div>
+        <MainNavigation />
       </aside>
     </>
   );
