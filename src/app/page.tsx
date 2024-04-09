09650550fc16a728be4f6f35883e619c0aed8b4f
Juliano Alves <contato@julianoalves.me>
2024-04-09 00:29:42 -0300

    feat: 🎸 add archetype form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,34 @@ export default function Home() {
     <>
       <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
       <SettingsTabs />
+      <div className="mt-6 flex flex-col ">
+        <div className="flex items-center justify-between pb-5 border-b  border-zinc-200">
+          <div className="space-y-1">
+            <h2 className="text-lg font-medium text-zinc-900">Personal info</h2>
+            <span className="text-sm text-zinc-500">
+              Update your photo and personal details here.
+            </span>
+          </div>
+          <div className="flex items-center gap-2">
+            <button
+              type="button"
+              className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm border border-zinc-300 text-zinc-700 hover:bg-zinc-50"
+            >
+              Cancel
+            </button>
+            <button
+              className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm bg-violet-600 text-white hover:bg-violet-700"
+              type="button"
+              form="settings"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+        <form id="settings" action="" className="mt-6 flex w-full flex-col">
+          form
+        </form>
+      </div>
     </>
   );
 }
