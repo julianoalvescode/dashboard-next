d59899b8924d791f368fb65bcce6cc3777ffb357
Juliano Alves <contato@julianoalves.me>
2024-04-09 02:48:05 -0300

    style: 💄 add animation select

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,16 @@ const config: Config = {
       maxWidth: {
         app: "700px",
       },
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: "0", transform: "translateY(-2px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        slideDownAndFade:
+          "slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+      },
       gridTemplateColumns: {
         app: "minmax(18rem, 20rem) 1fr",
         profile: "max-content 1fr min-content",
