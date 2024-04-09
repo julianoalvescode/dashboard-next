d5f2cc6bed99b3e30fb3758ec6241eda0d86b800
Juliano Alves <contato@julianoalves.me>
2024-04-09 01:07:09 -0300

    style: 💄 upload avatar style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { SettingsTabs } from "@/components/SettingsTabs";
 import * as Input from "@/components/Input";
-import { Mail } from "lucide-react";
+import { Mail, UploadCloud, User } from "lucide-react";
 
 export default function Home() {
   return (
@@ -74,7 +74,7 @@ export default function Home() {
 
           <div className="grid grid-cols-form gap-3 pt-5">
             <label
-              htmlFor="email"
+              htmlFor="photo"
               className="text-sm font-medium text-zinc-700"
             >
               Your photo
@@ -82,7 +82,31 @@ export default function Home() {
                 This will be displayed on your profile.
               </span>
             </label>
-            <div />
+            <div className="flex items-start gap-5">
+              <div className="bg-violet-50 flex h-16 w-16 items-center justify-center rounded-full">
+                <User className="w-8 h-8 text-violet-500" />
+              </div>
+              <label
+                htmlFor="photo"
+                className="group flex flex-1 cursor-pointer flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm hover:border-violet-200 hover:bg-violet-25 hover:text-violet-500"
+              >
+                <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100">
+                  <UploadCloud className="h-5 w-5 text-zinc-600 group-hover:text-violet-600" />
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                  <span className="text-sm">
+                    <span className="font-semibold text-violet-700">
+                      Click to upload
+                    </span>{" "}
+                    or drag and drop
+                  </span>
+                  <span className="text-xs">
+                    SVG, PNG, JPG or GIF (max. 800x400px)
+                  </span>
+                </div>
+              </label>
+              <input type="file" className="sr-only" id="photo" />
+            </div>
           </div>
 
           <div className="grid grid-cols-form gap-3 pt-5">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        juliano: "#FF0000",
-      },
       maxWidth: {
         app: "700px",
       },
@@ -18,6 +15,14 @@ const config: Config = {
         app: "minmax(18rem, 20rem) 1fr",
         profile: "max-content 1fr min-content",
         form: "minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)",
+      },
+      borderWidth: {
+        6: "6px",
+      },
+      colors: {
+        violet: {
+          25: "#fcfaff",
+        },
       },
     },
   },
